ff84dfaa4f90c32b1b0875ed308ba97e0a618a28
Mukeesh <72921651+Mukeesh@users.noreply.github.com>
2022-04-03 17:04:47 +0530

    Update test_quiz_operations.c
    file updated

--- 3_Implementation/test/test_quiz_operations.c
+++ 3_Implementation/test/test_quiz_operations.c
@@ -1,6 +1,12 @@
 #include "unity.h"
 #include "quiz_operations.h"
 #define PROJECT_NAME "QUIZ_PROJECT"
+/* Prototypes for all the test functions */
+
+/**
+ * @brief Testing function for Quiz_Project
+ * 
+ */
 void test_initArray();
 void test_insertArray();
 void test_freeArray();
@@ -12,11 +18,16 @@ void test_game();
 void test_XMLParser();
 void test_printRound();
 int test_printResult();
+/* Required by the unity test framework */
 void setUp(){}
+/* Required by the unity test framework */
 void tearDown(){}
+/* Start of the application test */
 int main()
 {
+    /* Initiate the Unity Test Framework */
     UNITY_BEGIN();
+    /* Run Test functions */
     RUN_TEST(test_initArray);
     RUN_TEST(test_insertArray);
     RUN_TEST(test_freeArray);
@@ -28,6 +39,7 @@ int main()
     RUN_TEST(test_XMLParser);
     RUN_TEST(test_printRound);
     RUN_TEST(test_printResult);
+    /* Close the Unity Test Framework */
     return UNITY_END;
     
 }
