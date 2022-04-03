e92ef0f74da81c4007567634ff81c6f1006e6fc1
Mukeesh <72921651+Mukeesh@users.noreply.github.com>
2022-04-03 16:50:10 +0530

    Update quiz_operations.c
    file updated

--- 3_Implementation/src/quiz_operations.c
+++ 3_Implementation/src/quiz_operations.c
@@ -1,3 +1,13 @@
+/**
+ * @file quiz_operations.c
+ * @author Mukeesh K(mmuki651@gmail.com)
+ * @brief This is the source code which contains all the functions for the project
+ * @version 0.1
+ * @date 2022-03-26
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include "quiz_operations.h"
 void initArray()
 {
