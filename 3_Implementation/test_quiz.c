87881ed25c0de027896c9dd12a726a0965af0c4e
Mukeesh <72921651+Mukeesh@users.noreply.github.com>
2022-04-03 17:36:30 +0530

    Update test_quiz.c
    File Updated

--- 3_Implementation/test_quiz.c
+++ 3_Implementation/test_quiz.c
@@ -1,13 +1,18 @@
 #include "unity.h"
 
 #define PROJECT_NAME "QUIZ_PROJECT"
+/* Prototypes for all the test functions */
 
+/**
+ * @brief Testing function for Quiz_Project
+ * 
+ */
 extern void test_quiz();
-
+/* Required by the unity test framework */
 void setUp(void){}
-
+/* Required by the unity test framework */
 void tearDown(void){}
-
+/* Start of the application test */
 int main(void){
     UnityBegin(NULL);
 
